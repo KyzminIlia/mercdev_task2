c7ba27a6d025a5a245df53dfb8dea3055dc29ea0
Kuzmin Ilia <i.kyzmin.j@gmail.com>
2014-07-15 14:26:05 +0400

    Initialisation

---
+++ FileDownloader/src/com/example/filedownloader/DownloadActivity.java
@@ -0,0 +1,12 @@
+package com.example.filedownloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DownloadActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_download);
+	}
+}
