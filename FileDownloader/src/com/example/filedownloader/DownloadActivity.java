c400a02f29e1cda261b9d56d6974da3ae6f05f81
KyzminIlia <i.kyzmin.j@gmail.com>
2014-07-16 00:54:42 +0400

    где то здесь упало

--- FileDownloader/src/com/example/filedownloader/DownloadActivity.java
+++ FileDownloader/src/com/example/filedownloader/DownloadActivity.java
@@ -1,12 +1,60 @@
 package com.example.filedownloader;
 
 import android.app.Activity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Intent;
+import android.content.Loader;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class DownloadActivity extends Activity {
+public class DownloadActivity extends Activity implements LoaderCallbacks<Bitmap> {
+	ProgressBar pb;
+	ImageLoader imgLoader;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_download);
+		pb  =(ProgressBar) findViewById(R.id.downloadPB);
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	public void btnClick(View v) {
+		Button btnDownload = (Button) v;
+		btnDownload.setEnabled(false);
+			
+	}
+
+	@Override
+	public Loader<Bitmap> onCreateLoader(int id, Bundle args) {
+		imgLoader = new ImageLoader(this,getString(R.string.image_URL));	
+		imgLoader.setHandler(handler);
+		return null;
+	}
+	private Handler handler = new Handler(){
+		public void handleMessage(Message msg) {
+			if (msg!=null){
+			int value =msg.getData().getInt("download");
+			pb.setProgress(value);
+			}
+			}
+	};
+	
+	@Override
+	public void onLoadFinished(Loader<Bitmap> arg0, Bitmap arg1) {
+		// TODO Auto-generated method stub
+		startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("/sdcard/downloadedImage.png")));
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Bitmap> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
